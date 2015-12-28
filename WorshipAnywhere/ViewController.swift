fb69ac7a529710cb84c9061df149b739e3f2f2c4
Mark Gruetzner <mgruetzner@rw3.com>
2015-12-28 15:44:49 -0600

    Initial Commit

---
+++ WorshipAnywhere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorshipAnywhere
+//
+//  Created by Mark Gruetzner on 12/28/15.
+//  Copyright © 2015 XNI Technologies, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
