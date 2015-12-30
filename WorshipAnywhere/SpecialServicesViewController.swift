5a0760948905a02f18805331e885f5977705edaf
Mark Gruetzner <mgruetzner@rw3.com>
2015-12-30 12:26:21 -0600

    add main collection views

---
+++ WorshipAnywhere/SpecialServicesViewController.swift
@@ -0,0 +1,89 @@
+//
+//  SpecialServicesViewController.swift
+//  WorshipAnywhere
+//
+//  Created by Mark Gruetzner on 12/30/15.
+//  Copyright © 2015 XNI Technologies, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SpecialServicesViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UIScrollViewDelegate {
+   
+   @IBOutlet var scrollView : UIScrollView!
+   @IBOutlet var collectionView : UICollectionView!
+   let reuseIdentifierFeatured = "VideoThumbnailCellForSpecialServices"
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      self.collectionView.delegate = self;
+      self.collectionView.dataSource = self;
+      // Do any additional setup after loading the view.
+   }
+   
+   override func viewDidLayoutSubviews() {
+      self.scrollView!.contentSize = CGSizeMake(1920, 2200)
+   }
+   
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+   
+   // Collection View Methods
+   
+   func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+      return 50
+   }
+   
+   func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+      return 50
+   }
+   
+   func collectionView(collectionView: UICollectionView,
+      layout collectionViewLayout: UICollectionViewLayout,
+      insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+         return UIEdgeInsets(top: 0.0, left: 50.0, bottom: 0.0, right: 50.0)
+   }
+   
+   func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+      return 1
+   }
+   
+   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+   {
+      if (collectionView == self.collectionView)
+      {
+         return 8
+      }
+      
+      return 0
+   }
+   
+   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+      
+      if (collectionView == self.collectionView)
+      {
+         let cell : VideoThumbnailCell = collectionView.dequeueReusableCellWithReuseIdentifier(self.reuseIdentifierFeatured, forIndexPath: indexPath) as! VideoThumbnailCell
+         
+         let  imageFilename = "VTS_01_1_Thumb.jpg"
+         cell.thumbnailImage.image = UIImage(named: imageFilename)
+         
+         return cell
+      }
+      
+      return UICollectionViewCell()
+   }
+   
+   /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+   
+}
--- WorshipAnywhere/VideoThumbnailCell.swift
+++ WorshipAnywhere/VideoThumbnailCell.swift
@@ -9,5 +9,53 @@
 import UIKit
 
 class VideoThumbnailCell: UICollectionViewCell {
-    
+   
+   @IBOutlet weak var thumbnailImage: UIImageView!
+   @IBOutlet weak var thumbnailLabel: UILabel!
+   
+   override init(frame: CGRect)
+   {
+      super.init(frame: frame)
+      self.commonInit()
+   }
+   
+   required init?(coder aDecoder: NSCoder)
+   {
+      super.init(coder: aDecoder)
+   }
+   
+   private func commonInit()
+   {
+      // Initialization code
+      
+      self.layoutIfNeeded()
+      self.layoutSubviews()
+      self.setNeedsDisplay()
+   }
+   
+   override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
+      if (self.focused)
+      {
+         self.thumbnailImage.adjustsImageWhenAncestorFocused = true
+         self.thumbnailLabel.hidden = false
+      }
+      else
+      {
+         self.thumbnailImage.adjustsImageWhenAncestorFocused = false
+         self.thumbnailLabel.hidden = false
+      }
+   }
+   
+   override func layoutSubviews()
+   {
+      super.layoutSubviews()
+   }
+   
+   override func awakeFromNib() {
+      super.awakeFromNib()
+   }
+   
+   override func prepareForReuse() {
+      super.prepareForReuse()
+   }
 }
